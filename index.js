c4008256a32e9d71038fffc77f69bd6577399947
sagudec <sagudelo.cano@gmail.com>
2017-03-14 23:55:13 -0500

    CRUD servicios

--- index.js
+++ index.js
@@ -27,16 +27,16 @@ app.use(bodyParser.json()) //para permitir peticiones en formato json
 
 //********************************************SERVICIOS****************************************
 
-app.post('/SISCOM/usuarios', (req, res)=> {
+app.post('/SISCOM/servicios', (req, res)=> {
 
 	// para poder ver el cuerpo de la peticion
 	console.log(req.body)
-	let newUsuario = Usuario(req.body);
-	newUsuario.save(function(error,createdusuarios){
+	let newServicio = Servicios(req.body);
+	newServicio.save(function(error,createdServicio){
 		if (error) throw error;
-		if (createdusuarios){
-			return res.status(200).json({message: 'EL usuario se ha recibido',
-				createdusuarios:createdusuarios});
+		if (createdServicio){
+			return res.status(200).json({message: 'EL servicio se ha recibido',
+				createdServicio:createdServicio});
 		}
 		return res.status(500).json({message: 'Error desconocido'});
 	})
@@ -48,10 +48,10 @@ app.post('/SISCOM/usuarios', (req, res)=> {
 
 // TIPO GET, PARA QUE MUESTRE LOS SERVICIOS
 
-app.get('/SISCOM/usuarios', (req, res) => {
-	Usuarios.find({},function(error,usuarios){
+app.get('/SISCOM/servicios', (req, res) => {
+	Servicios.find({},function(error,servicios){
 		if (error) throw error;	
-		return res.status(200).json(usuarios)
+		return res.status(200).json(servicios)
 
 	})
 	
@@ -61,10 +61,10 @@ app.get('/SISCOM/usuarios', (req, res) => {
 
 
 // GET PARA UNO ESPECIFICO POR ID
-app.get('/SISCOM/usuarios/:usuariosId', (req, res)=> {
-	Usuarios.findById(req.params.usuariosId, function(error,usuarios){
+app.get('/SISCOM/servicios/:servicioId', (req, res)=> {
+	Servicios.findById(req.params.servicioId, function(error,motivos){
 		if (error) throw error;	
-		return res.status(200).json(usuarios)
+		return res.status(200).json(servicios)
 })
 
 })
@@ -73,14 +73,14 @@ app.get('/SISCOM/usuarios/:usuariosId', (req, res)=> {
 // TIPO PUT, 'use strict'
 // modelo para guardar los productos en mongo
 
-app.put('/SISCOM/usuarios/:usuariosId', (req, res)=> {
+app.put('/SISCOM/servicios/:servicioId', (req, res)=> {
 
 	console.log(req.body)
-	Usuarios.findOneAndUpdate({_id:req.params.usuariosId},{$set:req.body},{new:true},function(error,updatedUsuarios){
+	Servicios.findOneAndUpdate({_id:req.params.servicioId},{$set:req.body},{new:true},function(error,updatedServicio){
 		if (error) throw error;
-		if (updatedUsuarios){
-			return res.status(201).json({message: 'El usuario se ha actualizado',
-				updatedUsuarios:updatedUsuarios});
+		if (updatedServicio){
+			return res.status(201).json({message: 'El servicio se ha actualizado',
+				updatedServicio:updatedServicio});
 		}
 		return res.status(500).json({message: 'Error desconocido'});
 	})
@@ -90,17 +90,16 @@ app.put('/SISCOM/usuarios/:usuariosId', (req, res)=> {
 
 // TIPO DELETE
 
-app.delete('/SISCOM/usuarios/:usuariosId', (req, res) => {
+app.delete('/SISCOM/servicios/:servicioId', (req, res) => {
 
-	Usuarios.find({_id:req.params.usuariosId}).remove().exec(function(error,data){
+	Servicios.find({_id:req.params.servicioId}).remove().exec(function(error,data){
 		if (error) throw error;
-		return res.status(200).json({message: 'El usuario se ha eliminado',
+		return res.status(200).json({message: 'El servicio se ha eliminado',
 			data:data});
 	
 			return res.status(500).json({message: 'Error desconocido'});
 	})
 })
-
 
 app.listen(port,() => {
 console.log(`PROYECTO SISCOM corriendo en http://localhost:${port}`)
